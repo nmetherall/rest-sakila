083d8e2910b463d8bf3c91a1b9ddd892ff380927
unknown <nmetherall@gmail.com>
2019-12-20 20:41:57 -0700

    Prettier thought my code was ugly

--- api_v2/models/filmCategoryModel.js
+++ api_v2/models/filmCategoryModel.js
@@ -5,31 +5,33 @@ const db = require("../../mysqlConnection");
 
 //Helper Method to determine the direction of the relationship
 const targetBaseHelper = req => {
-    if(req.filmId) return{
-        baseId : db.escape(req.filmId),
-        baseTable : "film",
-        targetTable : "category"
-    }
-    //if a film Id was passed
-    else if(req.catId) return{
-        baseId : db.escape(req.catId),
-        baseTable : "category",
-        targetTable : "film"
-    }
-}
+  if (req.filmId)
+    return {
+      baseId: db.escape(req.filmId),
+      baseTable: "film",
+      targetTable: "category"
+    };
+  //if a film Id was passed
+  else if (req.catId)
+    return {
+      baseId: db.escape(req.catId),
+      baseTable: "category",
+      targetTable: "film"
+    };
+};
 
 //GET: SELECT ALL
 router.get("/", (req, res) => {
-    const tb = targetBaseHelper(req);
+  const tb = targetBaseHelper(req);
 
-    //SQL statement
-    const sql = `SELECT ${tb.targetTable}.* FROM film_category INNER JOIN ${tb.targetTable} ON ${tb.targetTable}.${tb.targetTable}_id = film_category.${tb.targetTable}_id WHERE film_category.${tb.baseTable}_id=${tb.baseId}`;
+  //SQL statement
+  const sql = `SELECT ${tb.targetTable}.* FROM film_category INNER JOIN ${tb.targetTable} ON ${tb.targetTable}.${tb.targetTable}_id = film_category.${tb.targetTable}_id WHERE film_category.${tb.baseTable}_id=${tb.baseId}`;
 
-    //database query
-    db.query(sql, (err, results) => {
-        if(err)console.log(err);
-        res.send(JSON.stringify({"status": 200, "error": null, "response": results}))
-    });
+  //database query
+  db.query(sql, (err, results) => {
+    if (err) console.log(err);
+    res.send(JSON.stringify({ status: 200, error: null, response: results }));
+  });
 });
 
 module.exports = router;
