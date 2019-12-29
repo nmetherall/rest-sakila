d22667bc7418731d2fd85a49c7318a08f3b92931
unknown <nmetherall@gmail.com>
2019-12-28 19:33:45 -0700

    Added Access-Control

--- server.js
+++ server.js
@@ -7,15 +7,24 @@ const apiv2 = require("./api_v2/api.js");
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.use("/api_v1", apiv1);
 server.use("/api_v2", apiv2);
 
 server.get("/", (req, res) => {
-	res.status(200).json({ hello: `World!` });
+  res.status(200).json({ hello: `World!` });
 });
 
 server.get("/api_v1", (req, res) => {
-	res.status(200).json({ hello: `This is my API` });
+  res.status(200).json({ hello: `This is my API` });
 });
 
 module.exports = server;
