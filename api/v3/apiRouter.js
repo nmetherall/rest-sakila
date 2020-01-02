3e6bb339e190a5b8f0b005dc10ddc5baded13cf4
Nicholas Metherall <nmetherall@gmail.com>
2020-01-02 14:20:10 -0700

    new router format

---
+++ api/v3/apiRouter.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+
+const mysqlConnection = require("../../mysqlConnection");
+let tables = mysqlConnection.tables;
+
+//Default message
+router.get("/", (req, res) => {
+  res.status(200).send("Hello! Welcome to api_v3");
+});
+
+//Default message
+router.get("/_tables", (req, res) => {
+  res.status(200).send(tables);
+});
+
+//I can reserve paths that will override the default model
+router.get("/RESERVED", (req, res) => {
+  res.status(200).send("RESERVED");
+});
+
+//generic bridge table model
+const bridgeTableModel = require("./models/bridgeTableModel");
+router.use(
+  "/:baseTable/:baseId/:targetTable",
+  (req, res, next) => {
+    req.baseTable = req.params.baseTable;
+    req.baseId = req.params.baseId;
+    req.targetTable = req.params.targetTable;
+    next();
+  },
+  bridgeTableModel
+);
+
+router.route("/:baseTable/:baseId/:targetTable")
+  .get()
+  .post();
+router.route("/:baseTable/:baseId/:targetTable/:targetId")
+
+//Default Model if no specific path defined in api
+const model = require("./models/defaultModel");
+router.use("/", model);
+
+router.route("/:table")
+  .get()
+  .post();
+router.route("/:table/:id")
+  .get()
+  .delete()
+  .put();
+
+module.exports = router;
